3b1a10eddf7dd16cfa4df816eb1ce516e539ceb1
Abdelrahman Amr Attia <63236981+AbdelrahmanAmrAtiia@users.noreply.github.com>
2024-03-11 12:00:02 +0200

    init project

---
+++ src/test/java/com/stc/system/management/SystemManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stc.system.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
